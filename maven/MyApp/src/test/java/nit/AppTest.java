ed0f4d69c2bf88c25c6b772d4eb8938ff37c9c13
Arvind Kumar <156841546+arvind00A@users.noreply.github.com>
2024-03-29 17:24:28 +0530

    initial commit

---
+++ maven/MyApp/src/test/java/nit/AppTest.java
@@ -0,0 +1,20 @@
+package nit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
